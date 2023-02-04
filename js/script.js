32e9ec5bf8c995501e97d40bef2fb03b7ca9d67e
srmnbelle <64080430+srmnbelle@users.noreply.github.com>
2023-02-04 22:43:46 +0800

    Working on the footer

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
-const btn = document.getElementById('menu-btn')
-const nav = document.getElementById('menu')
+const btn = document.getElementById('menu-btn');
+const nav = document.getElementById('menu');
 
 btn.addEventListener('click', () => {
     btn.classList.toggle('open')
     nav.classList.toggle('flex')
     nav.classList.toggle('hidden')
-})
+});
+
+ScrollReveal({
+    reset: true,
+    duration: 1000,
+    scale: 1.25,
+});
+
+ScrollReveal().reveal('.headline', {container: "#feature", delay: 200});
